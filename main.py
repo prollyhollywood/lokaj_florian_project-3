444f4cffcc8fcfcb55b3e0ed797660844c816662
prollyhollywood <72751673+prollyhollywood@users.noreply.github.com>
2021-01-17 15:31:20 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 while True:
     if input.sound_level() >= 90:
         light.set_all(color.rgb(255,0,0))
-    elif input.sound_level() < 90:
+    elif input.sound_level() <= 89:
+        light.set_all(color.rgb(255,255,0))
+    else:
         light.set_all(color.rgb(0,255,0))
--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 while (true) {
     if (input.soundLevel() >= 90) {
         light.setAll(color.rgb(255, 0, 0))
-    } else if (input.soundLevel() < 90) {
+    } else if (input.soundLevel() <= 89) {
+        light.setAll(color.rgb(255, 255, 0))
+    } else {
         light.setAll(color.rgb(0, 255, 0))
     }
     
